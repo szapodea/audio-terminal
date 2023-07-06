01e989554f68308292b4348e6d5881d45dc25afd
szapodea <szapodea@purdue.edu>
2023-07-05 18:06:19 -0700

    added commands (cd, mov, ., ..)

--- handleCommands.py
+++ handleCommands.py
@@ -3,6 +3,19 @@ import os
 def handleListNoFlags(text: str):
     if 'list' in text:
         text = text.replace('list', 'ls')
+    if 'change directory' in text:
+        text = text.replace('change directory', 'cd')
+    if 'change folder' in text:
+        text = text.replace('change folder', 'cd')
+    if 'move' in text:
+        text = text.replace('move', 'mov')
+    if 'root' in text:
+        text = text.replace('root', '.')
+    if 'back' in text:
+        text = text.replace('back', '..')
+    if 'dot dot' in text:
+        text = text.replace('dot dot', '..')
+
 
     system_call = os.system(text)
 
